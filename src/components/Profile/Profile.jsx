5d5ff739258deb48406853bd6125ad5e7ad65615
tmssd <67862896+tmssd@users.noreply.github.com>
2022-12-22 22:44:38 +0200

    update

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -21,7 +21,7 @@ const Profile = ({ user, toggleModal, loadUser }) => {
   }
 
   const onProfileUpdate = (data) => {
-    fetch(`http://smartbrain.thomassoard.com:3000/profile/${user.id}`, {
+    fetch(`https://smartbrain.thomassoard.com:3000/profile/${user.id}`, {
       method: 'post',
       headers: {
         'Content-Type': 'application/json',
